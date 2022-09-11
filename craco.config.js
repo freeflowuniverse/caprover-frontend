1885738289fc87c415aa688e6f4aae5df5df236c
ahmedhanafy725 <hanafya@incubaid.com>
2022-09-11 19:36:11 +0200

    Upgrade grid3_client to 1.4.3

--- craco.config.js
+++ craco.config.js
@@ -1,9 +1,37 @@
 // jshint esversion:6
 const resolvePlugin = require('./craco.webpack.resolve.plugin')
-
 module.exports = {
     webpack: {
-        plugins: [],
+        plugins: [
+         
+        ],
+        configure: {
+            module: {
+              rules: [
+                {
+                  test: /\.js$/,
+                  loader: require.resolve('@open-wc/webpack-import-meta-loader'),
+                },
+                {
+                    test: /\.m?js$/,
+                    include: /node_modules[/\\|]@polkadot/i,
+                    use: {
+                      loader: 'babel-loader',
+                      options: {
+                        presets: [
+                          '@babel/preset-env',
+                        ],
+                        plugins: [
+                          "@babel/plugin-proposal-private-methods",
+                          "@babel/plugin-proposal-class-properties",
+                          '@babel/plugin-proposal-object-rest-spread',
+                        ]
+                      }
+                    }
+                  },
+              ]
+            }
+          },
     },
     plugins: [
         { plugin: resolvePlugin, options: { preText: "will add resolve with fs polyfill to webpack config:" } }
